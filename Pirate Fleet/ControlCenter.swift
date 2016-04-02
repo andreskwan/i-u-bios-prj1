c26a5927673de492e5f82e8c2878d8d1b7d4864a
andreskwan <andreskwan@gmail.com>
2016-04-02 16:35:05 -0500

    PF1 - adding the whole fleet to the grid

--- Pirate Fleet/ControlCenter.swift
+++ Pirate Fleet/ControlCenter.swift
@@ -21,6 +21,16 @@ class ControlCenter {
     
     func addShipsAndMines(human: Human) {
 //        <#WRITE YOUR CODE HERE!#>
+        let mediumShip1 = Ship(length: 3, location: GridLocation(x: 0, y: 0), isVertical: false)
+        let mediumShip2 = Ship(length: 3, location: GridLocation(x: 7, y: 0), isVertical: true)
+        human.addShipToGrid(mediumShip1)
+        human.addShipToGrid(mediumShip2)
+        let smallShip = Ship(length: 2, location: GridLocation(x: 3,y: 0), isVertical: false)
+        human.addShipToGrid(smallShip)
+        let largeShip = Ship(length: 4, location: GridLocation(x: 0,y: 4), isVertical: false)
+        human.addShipToGrid(largeShip)
+        let extraLargeShip = Ship(length: 5, location: GridLocation(x: 0, y: 5), isVertical: false)
+        human.addShipToGrid(extraLargeShip)
     }
     
     func calculateFinalScore(gameStats: GameStats) -> Int {
